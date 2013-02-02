e96b6f3be708c02f8e3744f9ab8663b051d909a2
Marco Fischer <marcofischer.ch@gmail.com>
2013-02-02 15:53:49 +0100

    test source deactivated

--- MobileFood/src/com/mobilefood/activity/MainActivity.java
+++ MobileFood/src/com/mobilefood/activity/MainActivity.java
@@ -54,7 +54,7 @@ public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         
-        setContentView(R.layout.activity_main_test);
+        setContentView(R.layout.activity_main);
         setTitle("");
         this.act = MainActivity.this;
 //        this.textView = (TextView) findViewById(R.id.TextView01);
